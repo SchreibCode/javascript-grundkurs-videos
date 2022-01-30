f85e91cea2bbc58cd2c6a407789b07bf30d17588
marvinscheffold <m@marvinscheffold.com>
2022-01-30 15:08:48 +0000

    finished video 2 with exercise

--- 01-hello-world/live-coding.js
+++ 01-hello-world/live-coding.js
@@ -1,1 +1,6 @@
+/*
+ * Mit console.log(<irgendetwas>)
+ * kann man <irgendetwas> auf der Konsole anzeigen
+ */
+
 console.log("Hello World");
---
+++ 02-variablen/live-coding.js
@@ -0,0 +1,25 @@
+/*
+ * Die drei primitive Datentypen:
+ * string, number und boolean
+ */
+
+/*
+ * var, let und const
+ */
+
+/*
+ * Variablen Deklaration
+ */
+
+/*
+ * Variablen Zuweisung
+ */
+
+/*
+ * Variablen Neuzuweisung
+ */
+
+/*
+ * Rechnen mit JavaScript:
+ * +, -, *, /, %
+ */
