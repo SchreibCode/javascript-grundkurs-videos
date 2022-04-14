be1c581036db80c8681873f9b73c55ec3051782a
marvinscheffold <m@marvinscheffold.com>
2022-04-14 11:37:33 +0200

    vor video

---
+++ 07-schleifen/aufgabe.md
@@ -0,0 +1,46 @@
+# FizzBuzz
+
+## Aufgabenstellung
+
+Schreibe Code der auf der Konsole die Zahlen von 1 bis 30 durchgeht.<br>
+Für jede Zahl die ohne Rest durch drei teilbar ist drucke `Fizz` aus.<br>
+Für jede Zahl die ohne Rest durch fünf teilbar ist drucke `Buzz` aus.<br>
+Und für jede Zahl die ohne Rest durch drei und fünf teilbar ist drucke `FizzBuzz` aus.
+
+## Hilfestellung
+
+Auf deiner Konsole soll am Ende folgendes stehen:
+
+```
+1
+2
+Fizz
+4
+Buzz
+6
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
+Fizz
+22
+23
+Fizz
+Buzz
+26
+Fizz
+28
+29
+FizzBuzz
+
+```
---
+++ 07-schleifen/live-coding.js
@@ -0,0 +1,46 @@
+/*
+ * Mit JavaScript Schleifen wie while oder for kann man Code wiederholen ohne ihn selbst mehrmals zu schreiben.
+ * Dank Schleifen können wir uns an die wichtige Regel "Wiederhole dich nicht!" halten.
+ * "Wiederhole dich nicht!" oder Englisch "Don´t repeat yourself" bedeutet, dass man den selben Code nicht mehrmals schreiben sollte.
+ */
+
+console.log("Hallo");
+console.log("Hallo");
+console.log("Hallo");
+console.log("Hallo");
+console.log("Hallo");
+console.log("Hallo");
+console.log("Hallo");
+console.log("Hallo");
+console.log("Hallo");
+console.log("Hallo");
+
+/*
+ * Die while Schleife wird ausgeführt, so lange die Bedingung true ergibt.
+ *
+ * while(bedingung){
+ *
+ * }
+ */
+
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Die for Schleife funktioniert genau wie die while Schleife, blos hat sie noch zwei zusätzliche Helfer für uns parat.
+ * Die Initialisierung wird nur ein mal ganz am Anfang ausgeführt.
+ * Die Nachhut wird nach jedem Schleifendurchlauf ausgeführt.
+ *
+ * for(initialisierung; bedingung; nachhut){
+ *
+ * }
+ */
