83f23c88fd27aeca4ff9168382827f2ac8af6f91
marvinscheffold <m@marvinscheffold.com>
2022-04-23 14:58:53 +0200

    reduced complexity

--- 09-objekte/clipboard.js
+++ 09-objekte/clipboard.js
@@ -4,15 +4,7 @@ const porsche911Turbo = {
     marke: "Porsche",
     modell: "911 Turbo",
     kofferaumVorne: true,
-    laenge: 4535, // mm
-    breite: 1900, // mm
-    hoehe: 1303, // mm
-    hubraum: 3745, // cm3
     leistung: 580, // PS
-    drehmoment: 750, // Nm
-    gewicht: 1640, // kg
-    zylinder: 6, // Stück
-    kraftstoff: "Super Plus",
     preis: 194650, // €
     modellReihen: ["911", "964", "993", "996", "997", "991", "992"],
 };
@@ -23,15 +15,7 @@ const porsche911Turbo = {
     marke: "Porsche",
     modell: "911 Turbo",
     kofferaumVorne: true,
-    laenge: 4535, // mm
-    breite: 1900, // mm
-    hoehe: 1303, // mm
-    hubraum: 3745, // cm3
     leistung: 580, // PS
-    drehmoment: 750, // Nm
-    gewicht: 1640, // kg
-    zylinder: 6, // Stück
-    kraftstoff: "Super Plus",
     preis: 194650, // €
     modellReihen: ["911", "964", "993", "996", "997", "991", "992"],
     printInfo: function () {
